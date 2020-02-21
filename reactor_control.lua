ca0699ac15826ae6c43e16b8f762b8634a71981d
chaos511 <dwc5496@yahoo.com>
2020-02-21 09:52:14 -0600

    version 0.2.2

--- reactor_control.lua
+++ reactor_control.lua
@@ -15,45 +15,45 @@ function formatstr(instr)
 
 	end
 end
+
+function getcolor(inboolean)
+	if inboolean then
+		return colors.green
+	else
+		return colors.red
+	end
+end
+displayList={{},{}}
+displayListIndex=1;
+function displayStatus(name,value,color,mon,x,y)
+	displayList[displayListIndex]={
+		x1=x,
+		y1=y,
+		color1=colors.white,
+		write1=name,
+		color2=color,
+		write2=value
+	}
+	displayListIndex=displayListIndex+1
+end
+
+function updatedisplay(mon)
+	for index, data in ipairs(displayList) do
+		mon.setCursorPos(data.x1,data.y1)
+		mon.setTextColor(data.color1)
+		mon.write(data.write1)
+		mon.setTextColor(data.color2)
+		mon.write(data.write2)
+	end
+	displayListIndex=1;
+end
+
 while true do   
 	local reactor = peripheral.find("BigReactors-Reactor")
 	local monitor = peripheral.find("monitor")
 
-	monitor.clear()  
+  
 	local energyStored=reactor.getEnergyStored()
-
-	monitor.setCursorPos(1,1)
-	monitor.setTextColor(colors.white)
-	monitor.write("Active: ")
-	  
-	if reactor.getActive() then
-		monitor.setTextColor(colors.green)
-	else
-		monitor.setTextColor(colors.red)
-	end
-	monitor.write(reactor.getActive())
-	  
-	  
-	monitor.setCursorPos(1,2)
-	monitor.setTextColor(colors.white)
-	monitor.write("Energy Stored: ")
-	monitor.setTextColor(colors.green)
-	monitor.write(formatstr(energyStored))
-	local energyPercent=math.floor(energyStored/100000)
-	monitor.write("Rf ")
-	monitor.write(energyPercent)
-	monitor.write("%")
-
-	
-	local deltaes=energyStored-lastEnergyStored
-	local deltatime=os.time()-lasttime
-	monitor.setCursorPos(1,3)
-	monitor.setTextColor(colors.white)
-	monitor.write("Delta ES: ")
-	monitor.setTextColor(colors.green)
-	monitor.write(formatstr(deltaes/(deltatime*1000)))
-	monitor.write("Rf/t ")
-	
 	
 	controlRodLevel=math.max(0, math.min(100, controlRodLevel+ (energyPercent-50)/ki) +(deltaes/kd))
 	if energyPercent <2 then
@@ -62,22 +62,19 @@ while true do
 	if energyPercent > 98 then
 		controlRodLevel=100
 	end
-	monitor.setCursorPos(1,4)
-	monitor.setTextColor(colors.white)
-	monitor.write("Producing: ")
-	monitor.setTextColor(colors.green)
-	monitor.write(formatstr(reactor.getEnergyProducedLastTick()))
-	monitor.write("Rf/t ")
-	monitor.write(math.floor((100-controlRodLevel)*100)/100)
-	monitor.write("%")
 
-
-
-
+	displayStatus("Active: ",reactor.getActive(),getcolor(reactor.getActive()),monitor,1,1)
+	displayStatus("Energy Stored: ",formatstr(energyStored).."Rf "..energyPercent.."%",getcolor(energyPercent>0),monitor,1,2)
+	displayStatus("Delta ES: ",formatstr(deltaes/(deltatime*1000)).."Rf/t",getcolor(deltaes>0),monitor,1,3)
+	displayStatus("Producing: ",formatstr(reactor.getEnergyProducedLastTick()).."Rf/t "..math.floor((100-controlRodLevel)*100)/100.."%",colors.green,monitor,1,4)
+	monitor.clear()
+	updatedisplay(monitor)
+	
 	lastEnergyStored=energyStored
 	lasttime=os.time()
 	reactor.setAllControlRodLevels(math.floor(controlRodLevel))
 	sleep(1)
+
   
 end
 
